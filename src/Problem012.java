665a4d4e3fd90d8a187d6e8e3367815ee0f3848f
Josh Birch <joshuabirch@protonmail.com>
2025-04-24 16:43:05 +0100

    Start problem 12 "Highly Divisible Triangle Number"

---
+++ src/Problem012.java
@@ -0,0 +1,27 @@
+public class Problem012 {
+//    public static int nthTriangleNumber(int n) {
+//        int triangleNumber = 0;
+//
+//        for (int i = 0; i <= n; i++) {
+//            triangleNumber += i;
+//        }
+//
+//        return triangleNumber;
+//    }
+
+    public static int triangleNumberDivisors(int divisors) {
+        int divisorCount = 0;
+        int currentTriangleNumber = 1;
+
+        while (divisors > divisorCount) {
+            for (int i = 0; i < currentTriangleNumber; i++) {
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
